885f397b55374215a87d9b0a1d1cb734fb14bf7b
iancu ionut <iiancu85@gmail.com>
2018-12-18 12:56:24 +0200

    18.12.2018

---
+++ ConsoleApp3/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+       
+        // car: color, date, number, carClass
+        public Car(string name, string color, int date, double number, char carClass)
+        {
+            Name = name;
+            Color = color;
+            Date = date;
+            Number = number;
+            CarClass = carClass;
+        }
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Date { get; set; }
+        public double Number { get; set; }
+        public char CarClass { get; set; }
+
+        public void SetColor(string color)
+        {
+            Color = color;
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // car: color, date, number, class
+
+            Car Ferarri = new Car("Ferarri", "red", 2018, 123.45, 'A');
+            Car Lamborghini = new Car("Lamborghini", "black", 2017, 100.8, 'B');
+
+
+            List<string> carShop = new List<string>
+            {
+                Ferarri.Name,
+                Lamborghini.Name
+            };
+            List<string> carColors = new List<string>
+            {
+                Ferarri.Color,
+                Lamborghini.Color,
+                "yellow"
+            };
+
+            string carShopPrint = String.Join(", ", carShop);
+            Console.WriteLine("Available cars in shop: " + carShopPrint);
+            string carColorsPrint = String.Join("-", carColors);
+            Console.WriteLine("Available colors in shop: " + carColorsPrint);
+
+            string s = carColors[2];
+
+            Ferarri.SetColor(s);
+
+            carColors = new List<string>
+            {
+                Ferarri.Color,
+                Lamborghini.Color,
+                "green"
+            };
+            
+
+            carShopPrint = String.Join("-", carColors);
+            Console.WriteLine("Available colors in shop after repaint: " + carShopPrint);
+
+
+
+
+
+        }
+
+    }
+}
