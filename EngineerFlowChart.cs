87e6ba8081038c291ce179f5950c7f848ebc79b3
iancu ionut <iiancu85@gmail.com>
2019-01-02 21:57:42 +0200

    02.01.2019

---
+++ EngineerFlowChart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // engineer flowchart using only booleans
+    class EngineerFlowChart
+    {
+        static void Main(string[] args)
+        {
+            bool doesItMove = false;
+            bool shouldItLeft = false;
+            bool shouldItRight = false;
+
+            if (doesItMove == true)
+            {
+                if (shouldItRight == true)
+                {
+                    Console.WriteLine("No Problem");
+                }
+                else
+                {
+                    Console.WriteLine("Apply ductape");
+                }
+            }
+            else
+            {
+                if (shouldItLeft == true)
+                {
+                    Console.WriteLine("Apply WD-40");
+                }
+                else
+                {
+                    Console.WriteLine("No Problem");
+                }
+            }
+
+            
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
