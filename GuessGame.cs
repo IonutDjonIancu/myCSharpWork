4725c8eadc3c50fd6f44adf651fec25b550c3427
iancu ionut <iiancu85@gmail.com>
2019-01-04 22:36:17 +0200

    04.01.2019

---
+++ GuessGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GuessGame
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = "apothecary";
+            string guess = null;
+            int guessCounter = 4;
+
+            do
+            {
+                Console.Write("guess the word: ");
+                guess = Console.ReadLine();
+                guessCounter--;
+
+            }
+            while (guess != secretMessage && guessCounter>0);
+
+            if (guess.Equals(secretMessage))
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoDimensionalArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TwoDimensionalArray
+    {
+        static void Main(string[] args)
+        {
+            int[,,] numberGrid = 
+                { 
+                    { 
+                        { 1, 2 },
+                        { 3, 4 }
+                    }, 
+                    { 
+                        { 5, 6 }, 
+                        { 7, 8 }
+                    }
+                };
+            
+            Console.WriteLine(numberGrid[0,1,0]);
+            
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
