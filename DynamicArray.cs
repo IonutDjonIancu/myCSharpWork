8f94b68b1206cdf5966ac1192a2ffd312cc08010
Ionut Iancu <ionut.iancu@gameloft.com>
2019-03-25 17:20:12 +0200

    25.03.2019

---
+++ DynamicArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Udemy Tutorial";
+
+            Console.WriteLine("Give dimension of array:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] myArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Insert element {0}", i);
+                myArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int j in myArr)
+            {
+                Console.Write("[{0}] ", j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
